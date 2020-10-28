e380ecb149552c1c1d4abd58639affd49f624238
umergta <umergta@gmail.com>
2020-10-28 14:46:56 +0800

    fixing issue with react modules not loading

--- PetHaven/pet-haven1/server/index.js
+++ PetHaven/pet-haven1/server/index.js
@@ -5,8 +5,8 @@ const pool = require('./db');
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
-app.listen(5001, ()=>{
-    console.log("connected on port 5001");
+app.listen(5002, ()=>{
+    console.log("connected on port 5002");
 });
 
 /* ROUTES */
