56b2a8fe30524ef8e9b4c82a21f1fcfd5a4d4ac6
seaniy <28254509+seaniy@users.noreply.github.com>
2020-11-06 00:17:38 +0800

    Added trigger for check_leaves

--- PetHaven/pet-haven1/server/database.sql
+++ PetHaven/pet-haven1/server/database.sql
@@ -240,3 +240,52 @@ RETURNS VARCHAR AS
 ' BEGIN
 INSERT INTO caretakers VALUES(email); INSERT INTO ft_caretakers VALUES(email, password, name, 0); INSERT INTO pet_owners VALUES(email, password, name, NULL); RETURN email; END; '
 LANGUAGE plpgsql;
+
+
+-- Show top 5 ft_caretaker earners
+SELECT *
+FROM salaries s JOIN ft_caretakers ft ON s.email=ft.email
+WHERE date_trunc('month', s.payment_date) = date_trunc('month', (CURRENT_DATE))
+ORDER BY payment_amount DESC
+LIMIT 5;
+
+-- Show bottom 5 ft_caretaker performance
+SELECT *
+FROM salaries s LEFT JOIN ft_caretakers ft ON s.email=ft.email
+WHERE date_trunc('month', s.payment_date) = date_trunc('month', (CURRENT_DATE))
+ORDER BY payment_amount ASC
+LIMIT 5;
+
+-- Check whether new leave results in no 2x 150 consecutive days
+CREATE OR REPLACE FUNCTION check_leave()
+RETURNS TRIGGER AS $$
+DECLARE total_count INTEGER;
+BEGIN
+SELECT COUNT(*) INTO total_count
+FROM (
+    SELECT extract(day from (( SELECT MIN(start_date)
+            FROM takes_leaves
+            WHERE email=NEW.email) - date_trunc('year', CURRENT_DATE))) AS difference 
+    UNION ALL
+    SELECT table1.start_date-table1.end_date AS difference
+        FROM (SELECT leaves.end_date AS end_date, MIN(leaves2.start_date) AS start_date 
+            FROM takes_leaves AS leaves, takes_leaves AS leaves2
+            WHERE leaves2.start_date >= leaves.end_date
+            AND leaves.email = leaves2.email
+            AND leaves.email = NEW.email
+            GROUP BY leaves.end_date) AS table1
+    UNION ALL 
+    SELECT extract(day from ((date_trunc('year', CURRENT_DATE) + interval '1 year' - interval '1 day') - (SELECT MAX(end_date) FROM takes_leaves WHERE email=NEW.email))) AS difference) as bigtable WHERE bigtable.difference >= 150;
+IF total_count < 2 THEN
+RAISE EXCEPTION 'invalid leaves';
+DELETE FROM takes_leaves WHERE email = NEW.email AND start_date = NEW.start_date AND end_date = NEW.end_date;
+END IF;
+RETURN NEW;
+END; $$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER is_valid_leave
+AFTER INSERT
+ON takes_leaves
+FOR EACH ROW
+EXECUTE PROCEDURE check_leave();
