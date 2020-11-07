dcf6bc0aee3a16a4ec37f70679606d4606e2b46e
umergta <umergta@gmail.com>
2020-11-07 18:58:26 +0800

    made bids.js and makeBids componenet for pet owner

--- PetHaven/pet-haven1/server/routes/bids.js
+++ PetHaven/pet-haven1/server/routes/bids.js
@@ -5,10 +5,27 @@ const authorization = require("../middleware/authorization");
 /* ROUTES */
 router.post("/", authorization, async(req, res)=>{
     try {
-        const{ transfer_method, bid_price, payment_method, rating_given, is_successful, feedback, start_date, end_date, pet_category, advertisement_email, owner_email, pet_name } = req.body;
+        const{ transfer_method, bid_price, payment_method, rating_given, start_date, end_date, pet_category, email, pet_name } = req.body;
+        console.log(req.body);
+
+        const startDate = new Date(start_date).toLocaleDateString("sv-SE", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+        });
+
+        const endDate = new Date(end_date).toLocaleDateString("sv-SE", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+        });
+
+        console.log(startDate);
+        console.log(endDate);
+
         var query = "INSERT INTO bids_for VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)";
-        await pool.query(query, [transfer_method, bid_price, payment_method, rating_given, is_successful, feedback, start_date, end_date, pet_category, advertisement_email, owner_email, pet_name]);
-        res.json("successfully bidded");
+        const parseres = await pool.query(query, [transfer_method, bid_price, payment_method, 0, "false", "none", startDate, endDate, pet_category, email, req.user, pet_name]);
+        res.json(parseres);
     } catch (error) {
         console.error(error.messages);
     }
